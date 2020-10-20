49793e4c132b49e957d3f9c6c8bd0ff657e97a14
Rodrigo <rodrigoprado.pss@gmail.com>
2020-10-19 21:27:14 -0300

    Entrega 2 - Verde

--- particoes_de_equivalencia/src/main/java/UsuarioServiceApplication.java
+++ particoes_de_equivalencia/src/main/java/UsuarioServiceApplication.java
@@ -6,7 +6,16 @@ public class UsuarioServiceApplication {
 	public static void main(String[] args) {
 		
         System.out.println("Login do Usuario");
-       
+        Gson gson = new Gson();
 
-	}
+		UsuarioImplement usuario = new UsuarioImplement("rodrigo@gmail.com","senhaforte");	
+		if(usuario.logar(usuario)) {
+			System.out.println(gson.toJson("Login Correto!"));
+		}
+		else{
+			System.out.println(gson.toJson("Login Incorreto!"));
+		}
+      
+    }
+
 }
--- particoes_de_equivalencia/src/main/java/usuarioservice/model/UsuarioImplement.java
+++ particoes_de_equivalencia/src/main/java/usuarioservice/model/UsuarioImplement.java
@@ -1,6 +1,47 @@
 package usuarioservice.model;
 
+import java.util.List;
+
 public class UsuarioImplement implements Usuario {
 	
 	
+	private String email;
+	private String senha;
+
+	@Override
+	public boolean logar(UsuarioImplement usuario) {
+		
+		UsuarioService a = new UsuarioService();		
+		List<UsuarioImplement> lista = a.InstanciarUsers();
+		 
+		 
+		 
+		for(int i = 0; i < lista.size(); i++){
+			if(lista.get(i).getEmail().equals(usuario.getEmail()) && lista.get(i).getSenha().equals(usuario.getSenha())) 
+				return true;
+			} 
+         	return false;
+	}
+	
+	
+	public UsuarioImplement(String email, String senha) {
+	       this.email = email;
+	       this.senha = senha;
+	   }
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 }
